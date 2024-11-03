2b65ec4f0e9d4ca7be7793abc0598a1341990ea7
salmaan <salmaan@>
2024-11-03 10:01:27 +0300

    commit

---
+++ routes/taskRoutes.js
@@ -0,0 +1,21 @@
+
+const { getTasks, createTasks, updateTasks, deleteTasks } = require("../controllers/taskController");
+
+const taskRoutes=(req,res)=>{
+    if(req.method=='GET'){
+        getTasks(req, res);
+    }else if(req.method=='POST') {
+        createTasks(req, res);
+    }else if(req.method=='PATCH') {
+        updateTasks(req, res);
+    }else if(req.method=='DELETE') {
+        deleteTasks(req, res);
+    }else{
+        res.writeHead(404,'Not Found', {'content-type': 'application/json'})
+            res.end(JSON.stringify({
+                message:'Page Not Found'
+            }))
+      
+      }  
+}
+module.exports=taskRoutes;
